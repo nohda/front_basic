037ee1ed8c8fb2b59ee998f56833e059645a9044
노다설 <sia0940@gmail.com>
2021-02-07 17:11:35 +0900

    쇼핑리스트 코드 개선(중복되는 eventListener 제거)

--- to do list/toDoList.js
+++ to do list/toDoList.js
@@ -2,60 +2,50 @@ const items = document.querySelector(".items");
 const input = document.querySelector(".footer_input");
 const addBtn = document.querySelector(".footer_button");
 
-function onAdd(){
+function onAdd() {
     const text = input.value;
-    if(text === ''){
+    if (text === '') {
         input.focus();
         return;
     }
     const item = createItem(text);
     items.appendChild(item);
-    item.scrollIntoView({block: 'end'});
+    item.scrollIntoView({ block: 'end' });
     input.value = '';
     input.focus();
 }
 
-function createItem(text){
+let id = 0;//UUID
+function createItem(text) {
     const itemRow = document.createElement('li');
-    itemRow.setAttribute('class','item_row');
-    
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
-    
-    const name = document.createElement('span');
-    name.setAttribute('class','item_name');
-    name.innerText = text;
-
-    const deleBtn = document.createElement('button');
-    deleBtn.setAttribute('class','item_delete');
-    deleBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    
-    deleBtn.addEventListener('click',()=>{
-        items.removeChild(itemRow); 
-    })
-
-    const itemDivider = document.createElement('item_divider');
-    itemDivider.setAttribute('class','item_divider');
-
-    item.appendChild(name);    
-    item.appendChild(deleBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
-    items.appendChild(itemRow);
-
-    // items.appendChild(item);
-    // items.appendChild(itemDivider);
-
+    itemRow.setAttribute('class', 'item_row');
+    itemRow.setAttribute('data-id',id);
+    itemRow.innerHTML = `
+        <div class="item">
+            <span class="item_name">${text}</span>
+            <button class="item_delete">
+                <i class="fas fa-trash-alt"  data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item_divider"></div>`;
+    id ++;
     return itemRow;
 }
 
-addBtn.addEventListener('click',()=>{
+addBtn.addEventListener('click', () => {
     onAdd();
 })
 
-input.addEventListener('keypress',(event)=>{
-    if(event.key === 'Enter'){
+input.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
         onAdd();
+    }
+});
+
+items.addEventListener('click',event=>{
+    const id = event.target.dataset.id;
+    if(id){
+        const toBeDelete = document.querySelector(`.item_row[data-id="${id}"]`);
+        toBeDelete.remove();
     }
 });
